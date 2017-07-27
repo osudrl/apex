6614f013e54a896eb186dc6c0cbc5fbd8497e1e8
Pedro Morais <autranemorais@gmail.com>
2017-07-27 00:26:23 -0700

    refactor

--- dagger_cache.py
+++ dagger_cache.py
@@ -20,34 +20,33 @@ class DAgger():
         obs_dim = env.observation_space.shape[0]
         action_dim = env.action_space.shape[0]
 
-        dagger_init = True
         X, y = Tensor(trj_len, obs_dim), Tensor(trj_len, action_dim)
-        for t in range(trj_len):
-            expert_action = controller(env.cstate, 0, 1, 0)
-            obs, _, done, _ = env.step(expert_action)
-            X[t, :], y[t, :] = Tensor(obs), Tensor(expert_action)
 
+        dagger_init = True
         for d in range(dagger_itr):
             X_new, y_new = Tensor(trj_len, obs_dim), Tensor(trj_len, action_dim)
 
             obs = env.reset()
             for t in range(trj_len):
-                if dagger_init:
-                    dagger_init = False
-                    continue
-
                 obs_torch = Var(Tensor(obs[None, :]), requires_grad=False)
                 action = self.learner(obs_torch).data.numpy()[0]
 
                 expert_action = controller(env.cstate, 0, 1, 0)
-                obs, _, done, _ = env.step(action)
+                if dagger_init:
+                    obs, _, done, _ = env.step(expert_action)
+                    X[t, :], y[t, :] = Tensor(obs), Tensor(expert_action)
 
-                if done or t == trj_len - 1:
-                    X_new, y_new = X_new[0:t, :], y_new[0:t, :]
-                    X, y = torch.cat((X, X_new), 0), torch.cat((y, y_new), 0)
-                    break
+                else:
+                    obs, _, done, _ = env.step(action)
 
-                X_new[t, :], y_new[t, :] = Tensor(obs), Tensor(expert_action)
+                    if done:
+                        X_new, y_new = X_new[0:t, :], y_new[0:t, :]
+                        break
+
+                    X_new[t, :], y_new[t, :] = Tensor(obs), Tensor(expert_action)
+
+            if not dagger_init:
+                X, y = torch.cat((X, X_new), 0), torch.cat((y, y_new), 0)
 
             # TODO: add training class?
             BATCH_SIZE = 100
@@ -63,3 +62,5 @@ class DAgger():
                     running_loss += self.learner.fit(X_batch, y_batch)[0]
                     bar.next("DAgger iteration: %s / %s" % (d + 1, dagger_itr))
                 print(running_loss / len(dataloader))
+
+            dagger_init = False
