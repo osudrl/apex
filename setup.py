e5317b823ca97483ae089fdf39000a6a35507d70
yeshg <yesh.godse@gmail.com>
2019-08-13 11:04:02 -0700

    modified attribution

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 #REQUIRED_PACKAGES = ["torch==0.4", "gym", "numpy", "visdom"]
 
-setup(name='deeprl',
+setup(name='apex',
       version='0.1',
       description='Deep reinforcement learning for continuous control',
-      author='Pedro Morais',
-      author_email='autranemorais@gmail.com',
+      author='Pedro Morais, Yesh Godse',
+      author_email='autranemorais@gmail.com, yesh.godse@gmail.com',
       license='MIT',
       packages=find_packages(exclude=("cassie", "tests")),
       #install_requires=REQUIRED_PACKAGES
