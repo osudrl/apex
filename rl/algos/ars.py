ad4e0bd8bb961761692992f40fdb3a73a528f2b4
Jonah Siekmann <siekmanj@oregonstate.edu>
2019-09-28 19:15:26 -0700

    finished algorithm, finetuning for usability still needed

--- rl/algos/ars.py
+++ rl/algos/ars.py
@@ -1,55 +1,101 @@
 import time
 import numpy as np
-import os
+#import os
 import torch
+import ray
 
-class ARS:
-  def __init__(self, policy, env, step_size=0.02, std=0.0075, deltas=16, top_n=None, reward_shift=0):
+@ray.remote
+class ARS_process(object):
+  def __init__(self, policy_thunk, env_thunk, std=0.0075):
+    self.policy = policy_thunk()
+    self.env    = env_thunk()
+    self.param_shape = [x.shape for x in self.policy.parameters()]
     self.std = std
-    self.num_deltas = deltas
-    self.param_shape = [x.shape for x in policy.parameters()]
-    self.policy = policy
-    self.env = env
-    self.step_size = step_size
 
-    self.reward_shift = reward_shift
-    if top_n is None:
-      self.top_n = self.num_deltas
-    else:
-      self.top_n = top_n
-
-    for p in self.policy.parameters():
-      p.data = torch.zeros(p.shape)
+  def update_policy(self, new_params):
+    for p, new_p in zip(self.policy.parameters(), new_params):
+      p.data = new_p.data
 
   def generate_delta(self):
     delta = [torch.normal(0, self.std, shape) for shape in self.param_shape]
     return delta
 
-  def step(self, black_box):
-    deltas = []
-    rewards = []
-    for n in range(self.num_deltas):
-      d = self.generate_delta()
+  def rollout(self, current_params, black_box, rollouts=1):
+    self.update_policy(current_params)
+    self.delta = self.generate_delta()
 
-      for p, dp in zip(self.policy.parameters(), d):
+    ret = []
+    timesteps = 0
+    for _ in range(rollouts):
+      for p, dp in zip(self.policy.parameters(), self.delta):
         p.data += dp;
       r_pos = black_box(self.policy)
 
-      for p, dp in zip(self.policy.parameters(), d):
+      for p, dp in zip(self.policy.parameters(), self.delta):
         p.data -= 2*dp;
       r_neg = black_box(self.policy)
 
-      for p, dp in zip(self.policy.parameters(), d):
+      for p, dp in zip(self.policy.parameters(), self.delta):
         p.data += dp;
 
-      deltas.append([d, r_pos, r_neg])
+      if isinstance(r_pos, tuple):
+        timesteps += r_pos[1]
+        r_pos = r_pos[0]
 
-      rewards.append(r_pos)
-      rewards.append(r_neg)
-    
-    reward_std = np.std(rewards)
-    factor = self.step_size / (self.top_n * reward_std)
-    for d, r_pos, r_neg in deltas:
-      for p, dp in zip(self.policy.parameters(), d):
-        p.data += factor * (r_pos - r_neg) * dp
+      if isinstance(r_neg, tuple):
+        timesteps += r_neg[1]
+        r_neg = r_neg[0]
+      
+      ret.append({'delta': self.delta, 'r_pos': r_pos, 'r_neg': r_neg, 'timesteps': timesteps})
 
+    return ret
+
+class ARS:
+  def __init__(self, policy_thunk, env_thunk, step_size=0.02, std=0.0075, deltas=32, workers=4, top_n=None):
+    self.std = std
+    self.num_deltas = deltas
+    self.num_workers = workers
+    self.step_size = step_size
+    self.policy = policy_thunk()
+
+    if top_n is not None:
+      self.top_n = top_n
+    else:
+      self.top_n = deltas
+
+    if not ray.is_initialized():
+      ray.init()
+
+    self.workers = [ARS_process.remote(policy_thunk, env_thunk, std=std) for _ in range(workers)]
+
+  def step(self, black_box):
+    pid = ray.put(list(self.policy.parameters())) # place the current policy parameters in shared mem
+
+    rollouts = self.num_deltas // self.num_workers # number of rollouts per worker
+
+    rollout_ids = [w.rollout.remote(pid, black_box, rollouts) for w in self.workers] # do rollouts
+    results = ray.get(rollout_ids) # retrieve rollout results from pool
+
+    results = [item for sublist in results for item in sublist] # flattens list of lists
+
+    r_pos = [item['r_pos'] for item in results]
+    r_neg = [item['r_neg'] for item in results]
+    delta = [item['delta'] for item in results]
+    timesteps = sum([item['timesteps'] for item in results])
+
+    r_std = np.std(r_pos + r_neg)
+
+    # if use top performing directions
+    if self.top_n < self.num_deltas:
+      sorted_indices = np.argsort(np.maximum(r_pos, r_neg))
+      r_pos = r_pos[sorted_indices]
+      r_neg = r_neg[sorted_indices]
+      delta = delta[sorted_indices]
+
+    r_pos /= self.top_n * r_std
+    r_neg /= self.top_n * r_std
+
+    for r_p, r_n, d in zip(r_pos, r_neg, delta):
+      for param, d_param in zip(self.policy.parameters(), d):
+        param.data += self.step_size * (r_p - r_n) * d_param.data
+    return timesteps
--- run_ars.py
+++ run_ars.py
@@ -3,20 +3,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import gym
-timesteps = 0
+import time
+
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
 
 class Policy(nn.Module):
-	def __init__(self, state_dim, action_dim, hidden_size=32):
-		super(Policy, self).__init__()
+  def __init__(self, state_dim, action_dim, hidden_size=32):
+    super(Policy, self).__init__()
 
-		self.l1 = nn.Linear(state_dim, hidden_size)
-		#self.l2 = nn.Linear(hidden_size, hidden_size)
-		self.l3 = nn.Linear(hidden_size, action_dim)
-	
-	def forward(self, state):
-		a = F.relu(self.l1(state))
-		#a = F.relu(self.l2(a))
-		return torch.tanh(self.l3(a)) 
+    self.l1 = nn.Linear(state_dim, hidden_size)
+    self.l2 = nn.Linear(hidden_size, action_dim)
+
+    for p in self.parameters():
+      p.data = torch.zeros(p.shape)
+  
+  def forward(self, state):
+    a = self.l1(state)
+    return self.l2(a)
 
 
 def eval_fn(policy, env, visualize=False, reward_shift=0):
@@ -25,6 +30,7 @@ def eval_fn(policy, env, visualize=False, reward_shift=0):
   rollout_reward = 0
   done = False
 
+  timesteps = 0
   while not done:
     action = policy.forward(state).detach()
 
@@ -34,24 +40,48 @@ def eval_fn(policy, env, visualize=False, reward_shift=0):
     state, reward, done, _ = env.step(action)
     state = torch.tensor(state).float()
     rollout_reward += reward - reward_shift
-    global timesteps
     timesteps+=1
-  return rollout_reward
+  return rollout_reward, timesteps
 
-env    = gym.make("Hopper-v2")
-policy = Policy(env.observation_space.shape[0], env.action_space.shape[0])
-policy = policy.float()
-
-iters = 1000
-
-def train(policy, env):
-  algo = ARS(policy, env, deltas=100)
+def train(policy_thunk, env_thunk, iters=1000, print_output=False, reset_every=10):
+  algo = ARS(policy_thunk, env_thunk, deltas=64, step_size=0.005)
 
   def black_box(p):
     return eval_fn(p, env, reward_shift=1)
 
+  avg_reward = 0
+  timesteps = 0
   for i in range(iters):
-    algo.step(black_box)
-    print("iter {} reward {} timesteps {}".format(i, eval_fn(algo.policy, env), timesteps))
+    if not i % reset_every:
+      avg_reward = 0
+      print()
 
-train(policy, env)
+    start = time.time()
+    samples = algo.step(black_box)
+    elapsed = time.time() - start
+
+    reward, _ = eval_fn(algo.policy, env)
+
+    if print_output:
+      timesteps += samples
+      avg_reward += reward
+      secs_per_sample = 1000 * elapsed / samples
+      print("iter {:4d} | ret {:6.2f} | last {:3d} iters: {:6.2f} | {:0.4f}s per 1k steps | timesteps {:10n}".format(i+1, reward, (i%reset_every)+1, avg_reward/((i%reset_every)+1), secs_per_sample, timesteps), end="\r")
+
+if __name__ == "__main__":
+  iters = 1000
+  reset_every = 10
+
+  # wrapper function for creating parallelized envs
+  def env_thunk():
+    return gym.make("Hopper-v2")
+
+  with env_thunk() as env:
+    obs_space = env.observation_space.shape[0]
+    act_space = env.action_space.shape[0]
+
+  # wrapper function for creating parallelized policies
+  def policy_thunk():
+    return Policy(obs_space, act_space).float()
+
+  train(policy_thunk, env_thunk, print_output=True)
