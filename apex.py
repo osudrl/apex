9851c5770f7fc54dcb0411b63503cda845c92f76
Jonah Siekmann <siekmanj@oregonstate.edu>
2019-09-30 18:13:17 -0700

    Revert "finishing touches on logo"
    This reverts commit 9cb9e1a61547091dd3eb5c9d717ac6a6ad621540.

--- apex.py
+++ apex.py
@@ -1,24 +1,86 @@
 import gym
 
 class color:
-  END = '\033[0m'
-  BOLD = '\033[1m'
-  ORANGE = '\033[38;5;202m'
-  BLACK = '\033[38;5;240m'
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m\033[48m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+   L1 = '\033[38;5;194m'
+   L2 = '\033[38;5;193m'
+   L3 = '\033[38;5;192m'
+   L4 = '\033[38;5;191m'
+   L5 = '\033[38;5;198m'
+   L6 = '\033[38;5;198m'
+   L7 = '\033[38;5;197m'
+   L8 = '\033[38;5;214m'
+   L9 = '\033[38;5;202m'
+   L10 = '\033[38;5;240m'
+
+"""
+   \e[38;5;190m
 
 
-def print_logo(subtitle=""):
-  print()
-  print(color.BOLD + color.ORANGE +  "         .8.         " + color.BLACK + " 8 888888888o   " + color.ORANGE + "8 8888888888   `8.`8888.      ,8' ")
-  print(color.BOLD + color.ORANGE +  "        .888.        " + color.BLACK + " 8 8888    `88. " + color.ORANGE + "8 8888          `8.`8888.    ,8' ")
-  print(color.BOLD + color.ORANGE +  "       :88888.       " + color.BLACK + " 8 8888     `88 " + color.ORANGE + "8 8888           `8.`8888.  ,8' ")
-  print(color.BOLD + color.ORANGE +  "      . `88888.      " + color.BLACK + " 8 8888     ,88 " + color.ORANGE + "8 8888            `8.`8888.,8' ")
-  print(color.BOLD + color.ORANGE +  "     .8. `88888.     " + color.BLACK + " 8 8888.   ,88' " + color.ORANGE + "8 888888888888     `8.`88888' ")
-  print(color.BOLD + color.ORANGE + "    .8`8. `88888.    " + color.BLACK  + " 8 888888888P'  " + color.ORANGE + "8 8888             .88.`8888. ")
-  print(color.BOLD + color.ORANGE + "   .8' `8. `88888.   " + color.BLACK  + " 8 8888         " + color.ORANGE + "8 8888            .8'`8.`8888. ")
-  print(color.BOLD + color.ORANGE + "  .8'   `8. `88888.  " + color.BLACK  + " 8 8888         " + color.ORANGE + "8 8888           .8'  `8.`8888. ")
-  print(color.BOLD + color.ORANGE + " .888888888. `88888. " + color.BLACK  + " 8 8888         " + color.ORANGE + "8 8888          .8'    `8.`8888. ")
-  print(color.BOLD + color.ORANGE + ".8'       `8. `88888." + color.BLACK  + " 8 8888         " + color.ORANGE + "8 888888888888 .8'      `8.`8888. " + color.END)
+
+
+\e[38;5;192m
+\e[38;5;193m
+\e[38;5;194m
+\e[38;5;195m
+\e[38;5;196m
+"""
+
+
+def print_logo(subtitle="", option=2):
+  classy = True
+  if not classy:
+    print()
+    print(color.BOLD + color.L9 + "          :::     :::::::::  :::::::::: :::    ::: " + color.END)
+    print(color.BOLD + color.L9 + "       :+: :+:   :+:    :+: :+:        :+:    :+: " + color.END)
+    print(color.BOLD + color.L9 + "     +:+   +:+  +:+    +:+ +:+         +:+  +:+ " + color.END)
+    print(color.BOLD + color.L9 + "   +#++:++#++: +#++:++#+  +#++:++#     +#++:+ " + color.END)
+    print(color.BOLD + color.L10 + "  +#+     +#+ +#+        +#+         +#+  +#+ " + color.END)
+    print(color.BOLD + color.L10 + " #+#     #+# #+#        #+#        #+#    #+# " + color.END)
+    print(color.BOLD + color.L10 + color.UNDERLINE    + "###     ### ###        ########## ###    ### " + color.END)
+  else:
+    if option == 1:
+      print(color.BOLD + color.L9 +  "         .8.         " + color.L10 + " 8 888888888o   " + color.L9 + "8 8888888888   `8.`8888.      ,8' ")
+      print(color.BOLD + color.L9 +  "        .888.        " + color.L10 + " 8 8888    `88. " + color.L9 + "8 8888          `8.`8888.    ,8' ")
+      print(color.BOLD + color.L9 +  "       :88888.       " + color.L10 + " 8 8888     `88 " + color.L9 + "8 8888           `8.`8888.  ,8' ")
+      print(color.BOLD + color.L9 +  "      . `88888.      " + color.L10 + " 8 8888     ,88 " + color.L9 + "8 8888            `8.`8888.,8' ")
+      print(color.BOLD + color.L9 +  "     .8. `88888.     " + color.L10 + " 8 8888.   ,88' " + color.L9 + "8 888888888888     `8.`88888' ")
+      print(color.BOLD + color.L10 + "    .8`8. `88888.    " + color.L9  + " 8 888888888P'  " + color.L10 + "8 8888             .88.`8888. ")
+      print(color.BOLD + color.L10 + "   .8' `8. `88888.   " + color.L9  + " 8 8888         " + color.L10 + "8 8888            .8'`8.`8888. ")
+      print(color.BOLD + color.L10 + "  .8'   `8. `88888.  " + color.L9  + " 8 8888         " + color.L10 + "8 8888           .8'  `8.`8888. ")
+      print(color.BOLD + color.L10 + " .888888888. `88888. " + color.L9  + " 8 8888         " + color.L10 + "8 8888          .8'    `8.`8888. ")
+      print(color.BOLD + color.L10 + ".8'       `8. `88888." + color.L9  + " 8 8888         " + color.L10 + "8 888888888888 .8'      `8.`8888. " + color.END)
+    if option == 2:
+      print(color.BOLD + color.L9 +  "         .8.         " + color.L10 + " 8 888888888o   " + color.L9 + "8 8888888888   `8.`8888.      ,8' ")
+      print(color.BOLD + color.L9 +  "        .888.        " + color.L10 + " 8 8888    `88. " + color.L9 + "8 8888          `8.`8888.    ,8' ")
+      print(color.BOLD + color.L9 +  "       :88888.       " + color.L10 + " 8 8888     `88 " + color.L9 + "8 8888           `8.`8888.  ,8' ")
+      print(color.BOLD + color.L9 +  "      . `88888.      " + color.L10 + " 8 8888     ,88 " + color.L9 + "8 8888            `8.`8888.,8' ")
+      print(color.BOLD + color.L9 +  "     .8. `88888.     " + color.L10 + " 8 8888.   ,88' " + color.L9 + "8 888888888888     `8.`88888' ")
+      print(color.BOLD + color.L9 + "    .8`8. `88888.    " + color.L10  + " 8 888888888P'  " + color.L9 + "8 8888             .88.`8888. ")
+      print(color.BOLD + color.L9 + "   .8' `8. `88888.   " + color.L10  + " 8 8888         " + color.L9 + "8 8888            .8'`8.`8888. ")
+      print(color.BOLD + color.L9 + "  .8'   `8. `88888.  " + color.L10  + " 8 8888         " + color.L9 + "8 8888           .8'  `8.`8888. ")
+      print(color.BOLD + color.L9 + " .888888888. `88888. " + color.L10  + " 8 8888         " + color.L9 + "8 8888          .8'    `8.`8888. ")
+      print(color.BOLD + color.L9 + ".8'       `8. `88888." + color.L10  + " 8 8888         " + color.L9 + "8 888888888888 .8'      `8.`8888. " + color.END)
+    if option == 3:
+      print(color.BOLD + color.L9 +  "         .8.         " + color.L10 + " 8 888888888o   " + color.L9 + "8 8888888888   `8.`8888.      ,8' ")
+      print(color.BOLD + color.L9 +  "        .888.        " + color.L10 + " 8 8888    `88. " + color.L9 + "8 8888          `8.`8888.    ,8' ")
+      print(color.BOLD + color.L9 +  "       :88888.       " + color.L10 + " 8 8888     `88 " + color.L9 + "8 8888           `8.`8888.  ,8' ")
+      print(color.BOLD + color.L9 +  "      . `88888.      " + color.L10 + " 8 8888     ,88 " + color.L9 + "8 8888            `8.`8888.,8' ")
+      print(color.BOLD + color.L9 +  "     .8. `88888.     " + color.L10 + " 8 8888.   ,88' " + color.L9 + "8 888888888888     `8.`88888' ")
+      print(color.BOLD + color.L10 + "    .8`8. `88888.    " + color.L9  + " 8 888888888P'  " + color.L10 + "8 8888             .88.`8888. ")
+      print(color.BOLD + color.L10 + "   .8' `8. `88888.   " + color.L9  + " 8 8888         " + color.L10 + "8 8888            .8'`8.`8888. ")
+      print(color.BOLD + color.L10 + "  .8'   `8. `88888.  " + color.L9  + " 8 8888         " + color.L10 + "8 8888           .8'  `8.`8888. ")
+      print(color.BOLD + color.L10 + " .888888888. `88888. " + color.L9  + " 8 8888         " + color.L10 + "8 8888          .8'    `8.`8888. ")
+      print(color.BOLD + color.L10 + ".8'       `8. `88888." + color.L9  + " 8 8888         " + color.L10 + "8 888888888888 .8'      `8.`8888. " + color.END)
   print("\n")
   print(subtitle)
   print("\n")
