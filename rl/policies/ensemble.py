3042ce2fd85c8811b0beb4b58c84db2a29646b22
siekmanj <siekmanj@oregonstate.edu>
2019-10-03 11:12:06 -0700

    initial refactor - removing old files

--- rl/policies/ensemble.py
+++
@@ -1,7 +0,0 @@
-class EnsemblePolicy:
-    def __init__(self, vpolicy):
-        self.vpolicy = vpolicy # policy vector aka a list of torch models
-
-    # take average action over ensemble of actions
-    def act(self, x, deterministic=False):
-        return None, sum([policy.act(x, deterministic)[1] for policy in self.vpolicy]) / len(self.vpolicy)
