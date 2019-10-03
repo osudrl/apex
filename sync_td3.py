dac424b0fc6c3633624cb69a0dc3a4b4f32fb1d7
yeshg <yesh.godse@gmail.com>
2019-10-02 17:54:31 -0700

    changed default env

--- sync_td3.py
+++ sync_td3.py
@@ -151,7 +151,7 @@ if __name__ == "__main__":
         env_fn = functools.partial(CassieEnv, "walking", clock_based=True, state_est=args.state_est)
         # env_fn = functools.partial(CassieEnv_speed_dfreq, "walking", clock_based = True, state_est=args.state_est)
         # env_fn = functools.partial(CassieIKEnv, clock_based=True, state_est=args.state_est)
-        print(env_fn().clock_inds)
+        # print(env_fn().clock_inds)
         obs_dim = env_fn().observation_space.shape[0]
         action_dim = env_fn().action_space.shape[0]
 
