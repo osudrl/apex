ae30695a2ceb6ead90fb164269dccc02fbc919d4
Jonah Siekmann <siekmanj@oregonstate.edu>
2019-10-08 15:38:09 -0700

    pushing latest changes

--- rl/algos/ddpg.py
+++ rl/algos/ddpg.py
@@ -1,11 +1,11 @@
+import pickle
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 import numpy as np
 
 from apex import gym_factory
-#import copy
 
 # Based on https://github.com/sfujim/TD3/blob/master/DDPG.py
 
@@ -28,17 +28,27 @@ class Critic_tmp(nn.Module):
   def __init__(self, state_dim, action_dim, hidden_size=256):
     super(Critic_tmp, self).__init__()
 
-    print("Creating critic with input {} + {} = {}".format(state_dim, action_dim, state_dim + action_dim))
-    self.l1 = nn.Linear(state_dim + action_dim, hidden_size)
+    #print("Creating critic with input {} + {} = {}".format(state_dim, action_dim, state_dim + action_dim))
+
+    self.state_linear = nn.Linear(state_dim, hidden_size)
+    self.action_linear = nn.Linear(action_dim, hidden_size)
+
+    #self.l1 = nn.Linear(state_dim + action_dim, hidden_size)
+
     self.l2 = nn.Linear(hidden_size, hidden_size)
     self.l3 = nn.Linear(hidden_size, 1)
 
     self.recurrent = False
 
   def forward(self, state, action):
-    x = torch.cat([state, action])
+    #print(state.shape, action.shape)
+    #x = torch.cat([state, action], dim=1)
     #print(x, x.size(), self.l1.in_features, self.l1.out_features)
-    x = F.relu(self.l1(x))
+
+    state = self.state_linear(state)
+    action = self.action_linear(action)
+
+    x = F.relu(torch.add(state, action))
     x = F.relu(self.l2(x))
     return self.l3(x)
 
@@ -68,7 +78,7 @@ class ReplayBuffer():
     self.size = min(self.size+1, self.max_size)
 
   def sample(self, batch_size):
-    idx = np.random.randint(0, self.size)
+    idx = np.random.randint(0, self.size, size=batch_size)
     return self.state[idx], self.action[idx], self.next_state[idx], self.reward[idx], self.not_done[idx]
 
 class DDPG():
@@ -76,20 +86,8 @@ class DDPG():
     self.behavioral_actor  = actor
     self.behavioral_critic = critic
 
-    import pickle
-
-    #self.target_actor  = copy.deepcopy(actor)
-    #self.target_critic = copy.deepcopy(critic)
-
     self.target_actor  = pickle.loads(pickle.dumps(actor))
     self.target_critic = pickle.loads(pickle.dumps(critic))
-    """
-    self.target_actor = type(actor)
-    self.target_actor.load_state_dict(actor.state_dict())
-
-    self.target_critic = type(critic)
-    self.target_critic.load_state_dict(critic.state_dict())
-    """
 
     self.actor_optimizer  = torch.optim.Adam(self.behavioral_actor.parameters(), lr=a_lr)
     self.critic_optimizer = torch.optim.Adam(self.behavioral_critic.parameters(), weight_decay=1e-2)
@@ -105,9 +103,7 @@ class DDPG():
     next_states = torch.Tensor(next_states)
     actions = torch.Tensor(actions)
 
-    #print("DOING TARGET CRITIC FORWARD")
     target_q = rewards + (not_dones * self.discount * self.target_critic(next_states, self.target_actor(next_states))).detach()
-    #print("DOING BEHAVIORAL CRITIC FORWARD")
     current_q = self.behavioral_critic(states, actions)
 
     critic_loss = F.mse_loss(current_q, target_q)
@@ -122,28 +118,21 @@ class DDPG():
     actor_loss.backward()
     self.actor_optimizer.step()
 
-    """
-    for param, target_param in zip(self.behavioral_critic.parameters(), self.target_critic.parameters()):
-      print("BEFORE UPDATE:", param.data[0])
-      break
-    """
     # Update the frozen target models
     for param, target_param in zip(self.behavioral_critic.parameters(), self.target_critic.parameters()):
       target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
-
-    """
-    for param, target_param in zip(self.behavioral_critic.parameters(), self.target_critic.parameters()):
-      print("AFTER UPDATE:", param.data[0])
-      break
-    """
 
     for param, target_param in zip(self.behavioral_actor.parameters(), self.target_actor.parameters()):
       target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
 def run_experiment(args):
 
+  torch.manual_seed(args.seed)
+  np.random.seed(args.seed)
+
   # wrapper function for creating parallelized envs
   env = gym_factory(args.env_name)()
+  env.seed(args.seed)
 
   obs_space = env.observation_space.shape[0]
   act_space = env.action_space.shape[0]
@@ -164,7 +153,6 @@ def run_experiment(args):
   while timesteps < args.timesteps:
 
     if timesteps > args.start_timesteps:
-      #print("doing policy actions")
       action_noise = np.random.normal(0, 0.2, size=act_space)
       action = algo.behavioral_actor.forward(torch.Tensor(state)).detach().numpy()
       action += action_noise
@@ -178,11 +166,10 @@ def run_experiment(args):
     episode_reward += reward
     episode_timesteps += 1
     
-    replay_buff.push(state, action, next_state, reward, done)
+    replay_buff.push(state, action, next_state, reward, 1 - done)
 
     if replay_buff.size > args.batch_size:
       algo.update_policy(replay_buff, batch_size=args.batch_size)
-
 
     if done:
       state = env.reset()
@@ -199,7 +186,7 @@ def run_experiment(args):
             action = algo.behavioral_actor.forward(torch.Tensor(state)).detach().numpy()
             state, reward, done, _ = env.step(action)
             eval_reward += reward
-        print("Episodes: {:4d} | evaluation: {:4.3f}\n".format(iter, eval_reward/10))
+        print("Episodes: {:4d} | Return: {:4.3f} | Timesteps {:n}\n".format(iter, eval_reward/10, timesteps))
         next_state = env.reset()
 
     timesteps += 1
