d7173b729a65a01bf7da096ce8cf304a20a8e148
yeshg <yesh.godse@gmail.com>
2019-12-15 17:41:00 -0800

    RL controller for aslip with delta done

--- RL_controller_aslip.py
+++ RL_controller_aslip.py
@@ -1,22 +1,25 @@
 from cassie.cassiemujoco.cassieUDP import *
 from cassie.cassiemujoco.cassiemujoco_ctypes import *
-from cassie.trajectory import CassieTrajectory
 # from cassie.speed_env import CassieEnv_speed
 # from cassie.speed_double_freq_env import CassieEnv_speed_dfreq
 # from cassie.speed_no_delta_env import CassieEnv_speed_no_delta
 # from cassie.speed_no_delta_neutral_foot_env import CassieEnv_speed_no_delta_neutral_foot
 
+from cassie.trajectory import CassieTrajectory
+
 import time
 import numpy as np
 import torch
 import pickle
-from rl.policies import GaussianMLP
 import platform
-from quaternion_function import *
+from cassie.quaternion_function import *
 
 #import signal 
 import atexit
-
+import sys
+import select
+import tty
+import termios
 
 """
 We need to include the trajectory library for the right offset information, as well as the right phaselen and speed
@@ -27,7 +30,7 @@ def getAllTrajectories(speeds):
 
     for i, speed in enumerate(speeds):
         dirname = os.path.dirname(__file__)
-        traj_path = os.path.join(dirname, "trajectory", "aslipTrajsImprovedCost_freq_correction", "walkCycle_{}.pkl".format(speed))
+        traj_path = os.path.join(dirname, "cassie", "trajectory", "aslipTrajsSweep", "walkCycle_{}.pkl".format(speed))
         trajectories.append(CassieIKTrajectory(traj_path))
 
     # print("Got all trajectories")
@@ -48,7 +51,7 @@ class CassieIKTrajectory:
 class TrajectoryInfo:
     def __init__(self):
         
-        self.speeds = [x / 10 for x in range(0, 21)]
+        self.speeds = [x / 10 for x in range(0, 31)]
         self.trajectories = getAllTrajectories(self.speeds)
         self.num_speeds = len(self.trajectories)
 
@@ -74,7 +77,7 @@ class TrajectoryInfo:
 
         # maybe make ref traj only send relevant idxs?
         ref_pos, ref_vel = self.get_ref_state(self.phase)
-        self.prev_action = ref_pos[self.pos_idx]
+        self.offset = ref_pos[self.pos_idx]
         self.phase_add = 1
 
     # get the corresponding state from the reference trajectory for the current phase
@@ -107,18 +110,19 @@ class TrajectoryInfo:
 
     def update_info(self, new_speed):
 
-        # new speed and trajectory
         self.speed = new_speed
-        self.trajectory = self.trajectories[ (np.abs(self.speeds - self.speed)).argmin() ]
+
+        # find closest speed in [0.0, 0.1, ... 3.0]. use this to find new trajectory
+        self.trajectory = self.trajectories[ (np.abs([speed_i - self.speed for speed_i in self.speeds])).argmin() ]
 
         # new offset
         ref_pos, ref_vel = self.get_ref_state(self.phase)
-        self.prev_action = ref_pos[self.pos_idx]
+        self.offset = ref_pos[self.pos_idx]
 
         # phaselen
         self.phaselen = self.trajectory.length - 1
 
-traj = TrajectoryInfo()
+        return self.phaselen, self.offset
 
 time_log   = [] # time stamp
 input_log  = [] # network inputs
@@ -129,9 +133,15 @@ target_log = [] #PD target log
 filename = "test.p"
 filep = open(filename, "wb")
 
+max_speed = 3.0
+min_speed = 0.0
+
 def log():
     data = {"time": time_log, "output": output_log, "input": input_log, "state": state_log, "target": target_log}
     pickle.dump(data, filep)
+
+def isData():
+    return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
 atexit.register(log)
 
@@ -141,18 +151,15 @@ torch.set_num_threads(1)
 # Prepare model
 # env = CassieEnv_speed_no_delta_neutral_foot("walking", clock_based=True, state_est=True)
 # env.reset_for_test()
-#
-speed = 0
+traj = TrajectoryInfo()
 
-# policy = torch.load("./trained_models/nodelta_neutral_StateEst_symmetry_speed0-3_freq1-2.pt")
 policy = torch.load("./trained_models/aslip_unified_freq_correction.pt")
 policy.eval()
 
 max_speed = 3.0
-# min_speed = -1.0
 min_speed = 0.0
-max_y_speed = 0.0
-min_y_speed = 0.0
+max_y_speed = 0.5
+min_y_speed = -0.5
 symmetry = True
 
 # Initialize control structure with gains
@@ -169,7 +176,7 @@ pos_index = np.array([2,3,4,5,6,7,8,9,14,15,16,20,21,22,23,28,29,30,34])
 vel_index = np.array([0,1,2,3,4,5,6,7,8,12,13,14,18,19,20,21,25,26,27,31])
 pos_mirror_index = np.array([2,3,4,5,6,21,22,23,28,29,30,34,7,8,9,14,15,16,20])
 vel_mirror_index = np.array([0,1,2,3,4,5,19,20,21,25,26,27,31,6,7,8,12,13,14,18])
-offset = traj.prev_action
+offset = traj.offset
 # offset = np.array([0.0045, 0.0, 0.4973, -1.1997, -1.5968, 0.0045, 0.0, 0.4973, -1.1997, -1.5968])
 
 # Determine whether running in simulation or on the robot
@@ -195,151 +202,149 @@ t = time.monotonic()
 t0 = t
 
 orient_add = 0
-while True:
-    # Wait until next cycle time
-    while time.monotonic() - t < 60/2000:
-        time.sleep(0.001)
-    t = time.monotonic()
-    tt = time.monotonic() - t0
-
-    # Get newest state
-    state = cassie.recv_newest_pd()
-
-    if state is None:
-        print('Missed a cycle')
-        continue	
-
-    if platform.node() == 'cassie':
-        # Radio control
-        orient_add -= state.radio.channel[3] / 60.0
-        curr_max = max_speed / 2# + (max_speed / 2)*state.radio.channel[4]
-        #print("curr_max:", curr_max)
-        speed_add = (max_speed / 2) * state.radio.channel[4]
-        speed = max(min_speed, state.radio.channel[0] * curr_max + speed_add)
-        speed = min(max_speed, state.radio.channel[0] * curr_max + speed_add)
-        
-        print("speed: ", speed)
-        traj.phase_add = 1+state.radio.channel[5]
-        # env.y_speed = max(min_y_speed, -state.radio.channel[1] * max_y_speed)
-        # env.y_speed = min(max_y_speed, -state.radio.channel[1] * max_y_speed)
-    else:
-        # Automatically change orientation and speed
+old_settings = termios.tcgetattr(sys.stdin)
+try:
+    tty.setcbreak(sys.stdin.fileno())
+    while True:
+        # Wait until next cycle time
+        while time.monotonic() - t < 60/2000:
+            time.sleep(0.001)
+        t = time.monotonic()
         tt = time.monotonic() - t0
-        orient_add += 0#math.sin(t / 8) / 400
-        #env.speed = 0.2
-        speed += 0.001#((math.sin(tt / 2)) * max_speed)
-        print("speed: ", speed)
-        #if env.phase % 14 == 0:
-        #	env.speed = (random.randint(-1, 1)) / 2.0
-        # print(env.speed)
-        speed = max(min_speed, speed)
-        speed = min(max_speed, speed)
-        # env.y_speed = (math.sin(tt / 2)) * max_y_speed
-        # env.y_speed = max(min_y_speed, env.y_speed)
-        # env.y_speed = min(max_y_speed, env.y_speed)
 
-    # if env.phase < 14 or symmetry is False:
-    	# quaternion = euler2quat(z=env.orientation, y=0, x=0)
-    	# iquaternion = inverse_quaternion(quaternion)
-    	# new_orientation = quaternion_product(iquaternion, state.pelvis.orientation[:])
-    # 	if new_orientation[0] < 0:
-    # 		new_orientation = -new_orientation
-    # 	new_translationalVelocity = rotate_by_quaternion(state.pelvis.translationalVelocity[:], iquaternion)
+        # Get newest state
+        state = cassie.recv_newest_pd()
 
-    # 	print('quaternion: {}, new_orientation: {}'.format(quaternion, new_orientation))
+        if state is None:
+            print('Missed a cycle')
+            continue	
 
-    # 	# Construct input vector
-    # 	if symmetry:
-    # 		cassie_state = np.copy(np.concatenate([[state.pelvis.position[2] - state.terrain.height], new_orientation[:], state.motor.position[:], new_translationalVelocity[:], state.pelvis.rotationalVelocity[:], state.motor.velocity[:], state.pelvis.translationalAcceleration[:], state.joint.position[:], state.joint.velocity[:]]))
-    # 	else:
-    # 		cassie_state = np.copy(np.concatenate([[state.pelvis.position[2] - state.terrain.height], new_orientation[:], state.motor.position[:], new_translationalVelocity[:], state.pelvis.rotationalVelocity[:], state.motor.velocity[:], state.pelvis.translationalAcceleration[:], state.leftFoot.toeForce[:], state.leftFoot.heelForce[:], state.rightFoot.toeForce[:], state.rightFoot.heelForce[:]]))
-    # 	ref_pos, ref_vel = env.get_kin_next_state()
-    # 	RL_state = np.concatenate([cassie_state, ref_pos[pos_index], ref_vel[vel_index]])
-    # else:
-    # 	quaternion = euler2quat(z=env.orientation, y=0, x=0)
-    # 	cassie_state = get_mirror_state(state, quaternion)
-    # 	ref_pos, ref_vel = env.get_kin_next_state()
-    # 	ref_vel[1] = -ref_vel[1]
-    # 	euler = quaternion2euler(ref_pos[3:7])
-    # 	euler[0] = -euler[0]
-    # 	euler[2] = -euler[2]
-    # 	ref_pos[3:7] = euler2quat(z=euler[2],y=euler[1],x=euler[0])
-    # 	RL_state = np.concatenate([cassie_state, ref_pos[pos_mirror_index], ref_vel[vel_mirror_index]])
+        if platform.node() == 'cassie':
+            # Radio control
+            orient_add -= state.radio.channel[3] / 60.0
+            traj.speed = max(min_speed, state.radio.channel[0] * max_speed)
+            traj.speed = min(max_speed, state.radio.channel[0] * max_speed)
+            traj.phase_add = state.radio.channel[5] + 1
+            # env.y_speed = max(min_y_speed, -state.radio.channel[1] * max_y_speed)
+            # env.y_speed = min(max_y_speed, -state.radio.channel[1] * max_y_speed)
+        else:
+            # Automatically change orientation and speed
+            tt = time.monotonic() - t0
+            orient_add += 0#math.sin(t / 8) / 400            
+            if isData():
+                c = sys.stdin.read(1)
+                if c == 'w':
+                    traj.speed += .1
+                    print("Increasing speed to: ", traj.speed)
+                elif c == 's':
+                    traj.speed -= .1
+                    print("Decreasing speed to: ", traj.speed)
+                # elif c == 'a':
+                #     y_speed += .1
+                #     print("Increasing y speed to: ", y_speed)
+                # elif c == 'd':
+                #     y_speed -= .1
+                #     print("Decreasing y speed to: ", y_speed)
+                elif c == 'j':
+                    traj.phase_add += .1
+                    print("Increasing frequency to: ", traj.phase_add)
+                elif c == 'h':
+                    traj.phase_add -= .1
+                    print("Decreasing frequency to: ", traj.phase_add)
+                elif c == 'l':
+                    orient_add += .1
+                    print("Increasing orient_add to: ", orient_add)
+                elif c == 'k':
+                    orient_add -= .1
+                    print("Decreasing orient_add to: ", orient_add)
+                elif c == 'p':
+                    print("Applying force")
+                    push = 100
+                    push_dir = 0
+                    force_arr = np.zeros(6)
+                    force_arr[push_dir] = push
+                    env.sim.apply_force(force_arr)
+                else:
+                    pass
+            traj.speed = max(min_speed, traj.speed)
+            traj.speed = min(max_speed, traj.speed)
+            # y_speed = max(min_y_speed, y_speed)
+            # y_speed = min(max_y_speed, y_speed)
+            print("speed: ", traj.speed)
+            # print("y_speed: ", y_speed)
+            print("frequency: ", traj.phase_add)
 
-    traj.update_info(speed)
+        traj.update_info(traj.speed)
 
-    # phaselen = 11 # used to be 27
-
-    clock = [np.sin(2 * np.pi *  traj.phase / traj.phaselen), np.cos(2 * np.pi *  traj.phase / traj.phaselen)]
-    # clock = [np.sin(2 * np.pi *  phase / 27 * (2000 / 30)), np.cos(2 * np.pi *  phase / 27 * (2000 / 30))]
-    # euler_orient = quaternion2euler(state.pelvis.orientation[:]) 
-    # print("euler orient: ", euler_orient + np.array([orient_add, 0, 0]))
-    # new_orient = euler2quat(euler_orient + np.array([orient_add, 0, 0]))
-    quaternion = euler2quat(z=orient_add, y=0, x=0)
-    iquaternion = inverse_quaternion(quaternion)
-    new_orient = quaternion_product(iquaternion, state.pelvis.orientation[:])
-    if new_orient[0] < 0:
-        new_orient = -new_orient
-    new_translationalVelocity = rotate_by_quaternion(state.pelvis.translationalVelocity[:], iquaternion)
-    print('new_orientation: {}'.format(new_orient))
-    print('clock: {}'.format(clock))
-        
-    # ext_state = np.concatenate((clock, [speed]))
-    ext_state = clock
-    robot_state = np.concatenate([
-            [state.pelvis.position[2] - state.terrain.height], # pelvis height
-            new_orient,
-            # state.pelvis.orientation[:],                                 # pelvis orientation
-            state.motor.position[:],                                     # actuated joint positions
-
-            # state.pelvis.translationalVelocity[:],                       # pelvis translational velocity
-            new_translationalVelocity[:],
-            state.pelvis.rotationalVelocity[:],                          # pelvis rotational velocity 
-            state.motor.velocity[:],                                     # actuated joint velocities
-
-            state.pelvis.translationalAcceleration[:],                   # pelvis translational acceleration
+        clock = [np.sin(2 * np.pi *  traj.phase / traj.phaselen), np.cos(2 * np.pi *  traj.phase / traj.phaselen)]
+        # euler_orient = quaternion2euler(state.pelvis.orientation[:]) 
+        # print("euler orient: ", euler_orient + np.array([orient_add, 0, 0]))
+        # new_orient = euler2quat(euler_orient + np.array([orient_add, 0, 0]))
+        quaternion = euler2quat(z=orient_add, y=0, x=0)
+        iquaternion = inverse_quaternion(quaternion)
+        new_orient = quaternion_product(iquaternion, state.pelvis.orientation[:])
+        if new_orient[0] < 0:
+            new_orient = -new_orient
+        new_translationalVelocity = rotate_by_quaternion(state.pelvis.translationalVelocity[:], iquaternion)
+        # print('new_orientation: {}'.format(new_orient))
             
-            state.joint.position[:],                                     # unactuated joint positions
-            state.joint.velocity[:]                                      # unactuated joint velocities
-    ])
-    RL_state = np.concatenate([robot_state, ext_state])
+        # ext_state = np.concatenate((clock, [speed, y_speed]))
+        ext_state = np.concatenate((clock, [traj.speed] ))
+        robot_state = np.concatenate([
+                [state.pelvis.position[2] - state.terrain.height], # pelvis height
+                new_orient,
+                # state.pelvis.orientation[:],                                 # pelvis orientation
+                state.motor.position[:],                                     # actuated joint positions
 
-    
-    #pretending the height is always 1.0
-    # RL_state[0] = 1.0
-    
-    # Construct input vector
-    torch_state = torch.Tensor(RL_state)
-    # torch_state = shared_obs_stats.normalize(torch_state)
+                state.pelvis.translationalVelocity[:],                       # pelvis translational velocity
+                # new_translationalVelocity[:],
+                state.pelvis.rotationalVelocity[:],                          # pelvis rotational velocity 
+                state.motor.velocity[:],                                     # actuated joint velocities
 
-    # Get action
-    action = policy.act(torch_state, True)
-    env_action = action.data.numpy()
-    target = env_action + offset
+                state.pelvis.translationalAcceleration[:],                   # pelvis translational acceleration
+                
+                state.joint.position[:],                                     # unactuated joint positions
+                state.joint.velocity[:]                                      # unactuated joint velocities
+        ])
+        RL_state = np.concatenate([robot_state, ext_state])
 
-    #print(state.pelvis.position[2] - state.terrain.height)
+        
+        #pretending the height is always 1.0
+        RL_state[0] = 1.0
+        
+        # Construct input vector
+        torch_state = torch.Tensor(RL_state)
+        # torch_state = shared_obs_stats.normalize(torch_state)
 
-    # Send action
-    for i in range(5):
-        u.leftLeg.motorPd.pTarget[i] = target[i]
-        u.rightLeg.motorPd.pTarget[i] = target[i+5]
-    #time.sleep(0.005)
-    cassie.send_pd(u)
+        # Get action
+        action = policy.act(torch_state, True)
+        env_action = action.data.numpy()
+        target = env_action + traj.offset
 
-    # Measure delay
-    print('delay: {:6.1f} ms'.format((time.monotonic() - t) * 1000))
+        #print(state.pelvis.position[2] - state.terrain.height)
 
-    # Logging
-    time_log.append(time.time())
-    state_log.append(state)
-    input_log.append(RL_state)
-    output_log.append(env_action)
-    target_log.append(target)
+        # Send action
+        for i in range(5):
+            u.leftLeg.motorPd.pTarget[i] = target[i]
+            u.rightLeg.motorPd.pTarget[i] = target[i+5]
+        #time.sleep(0.005)
+        cassie.send_pd(u)
 
-    # Track phase
-    
-    traj.phase += traj.phase_add
-    if traj.phase >= traj.phaselen + 1:
-    # if phase >= 28 * (2000 / 30):
-        traj.phase = 0
-        traj.counter += 1
+        # Measure delay
+        print('delay: {:6.1f} ms'.format((time.monotonic() - t) * 1000))
+
+        # Logging
+        time_log.append(time.time())
+        state_log.append(state)
+        input_log.append(RL_state)
+        output_log.append(env_action)
+        target_log.append(target)
+
+        # Track phase
+        traj.phase += traj.phase_add
+        if traj.phase >= traj.phaselen:
+            traj.phase = 0
+            traj.counter += 1
+
+finally:
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
