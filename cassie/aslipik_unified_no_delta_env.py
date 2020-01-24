5d4dfc66cc39a35b54a0f04327bf57ea904b35dd
yeshg <yesh.godse@gmail.com>
2020-01-24 15:17:02 -0800

    added v8 no_delta_env with full taskspace reward

--- cassie/aslipik_unified_no_delta_env.py
+++ cassie/aslipik_unified_no_delta_env.py
@@ -273,7 +273,6 @@ class UnifiedCassieIKEnvNoDelta:
 
         #weight = [.1] * 10
 
-        joint_error       = 0
         footpos_error     = 0
         com_vel_error     = 0
         action_penalty    = 0
@@ -297,20 +296,20 @@ class UnifiedCassieIKEnvNoDelta:
         # try to match com velocity
         ref_cvel = self.get_ref_com_vel(self.phase + 1)
 
-        # center of mass vel: x, y, z
-        cvel = self.cassie_state.pelvis.translationalVelocity
-        for j in [0, 1, 2]:
-            com_vel_error += np.linalg.norm(cvel[j] - ref_cvel[j])
+        # # center of mass vel: x, y, z
+        # cvel = self.cassie_state.pelvis.translationalVelocity
+        # for j in [0, 1, 2]:
+        #     com_vel_error += np.linalg.norm(cvel[j] - ref_cvel[j])
 
-        # each joint pos, skipping feet
-        for i, j in enumerate(self.reward_pos_idx):
-            target = ref_pos[j]
-            actual = qpos[j]
+        # # each joint pos, skipping feet
+        # for i, j in enumerate(self.reward_pos_idx):
+        #     target = ref_pos[j]
+        #     actual = qpos[j]
 
-            if j == 20 or j == 34:
-                joint_error += 0
-            else:
-                joint_error += (target - actual) ** 2
+        #     if j == 20 or j == 34:
+        #         joint_error += 0
+        #     else:
+        #         joint_error += (target - actual) ** 2
 
         # action penalty
         action_penalty = np.linalg.norm(action - self.prev_action)
@@ -323,21 +322,19 @@ class UnifiedCassieIKEnvNoDelta:
         if straight_diff < 0.05:
             straight_diff = 0
 
-        reward = 0.3 * np.exp(-joint_error) +       \
-                 0.15 * np.exp(-footpos_error) +    \
-                 0.15 * np.exp(-com_vel_error) +    \
-                 0.15 * np.exp(-action_penalty) +     \
-                 0.15 * np.exp(-foot_orient_penalty) + \
-                 0.1  * np.exp(-straight_diff)
+        reward = 0.2 * np.exp(-footpos_error) +    \
+                 0.2 * np.exp(-com_vel_error) +    \
+                 0.2 * np.exp(-action_penalty) +     \
+                 0.2 * np.exp(-foot_orient_penalty) + \
+                 0.2  * np.exp(-straight_diff)
 
         if self.debug:
-            print("reward: {12}\njoint:\t{0:.2f}, % = {1:.2f}\nfoot:\t{2:.2f}, % = {3:.2f}\ncom_vel:\t{4:.2f}, % = {5:.2f}\naction_penalty:\t{6:.2f}, % = {7:.2f}\nfoot_orient_penalty:\t{8:.2f}, % = {9:.2f}\nstraight_diff:\t{10:.2f}, % = {11:.2f}\n\n".format(
-            0.3  * np.exp(-joint_error),            0.3 * np.exp(-joint_error) / reward * 100,
-            0.15 * np.exp(-footpos_error),          0.15 * np.exp(-footpos_error) / reward * 100,
-            0.15 * np.exp(-com_vel_error),          0.15 * np.exp(-com_vel_error) / reward * 100,
-            0.15 * np.exp(-action_penalty),         0.15 * np.exp(-action_penalty) / reward * 100,
-            0.15 * np.exp(-foot_orient_penalty),    0.15 * np.exp(-foot_orient_penalty) / reward * 100,
-            0.1  * np.exp(-straight_diff),          0.1  * np.exp(-straight_diff) / reward * 100,
+            print("reward: {10}\nfoot:\t{0:.2f}, % = {1:.2f}\ncom_vel:\t{2:.2f}, % = {3:.2f}\naction_penalty:\t{4:.2f}, % = {5:.2f}\nfoot_orient_penalty:\t{6:.2f}, % = {7:.2f}\nstraight_diff:\t{8:.2f}, % = {9:.2f}\n\n".format(
+            0.2 * np.exp(-footpos_error),          0.2 * np.exp(-footpos_error) / reward * 100,
+            0.2 * np.exp(-com_vel_error),          0.3 * np.exp(-com_vel_error) / reward * 100,
+            0.2 * np.exp(-action_penalty),         0.2 * np.exp(-action_penalty) / reward * 100,
+            0.2 * np.exp(-foot_orient_penalty),    0.2 * np.exp(-foot_orient_penalty) / reward * 100,
+            0.2  * np.exp(-straight_diff),         0.2  * np.exp(-straight_diff) / reward * 100,
             reward
             )
             )
