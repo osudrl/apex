8c08eb325da64e28f263c7748fa04c0215c76a3b
JohnNWarila <jnw.csgo@gmail.com>
2020-02-04 14:04:06 -0800

    Documentation for validate.py

--- validate.py
+++ validate.py
@@ -1,12 +1,23 @@
-#validate the sensitivity of parameters
+#validate the sensitivity of policy to environment parameters
 
 ##########To do#########
 #Formalize desired output
 #Get to run with user input of env and policy without iteration
 #Iterate through each parameter
 
-def iterativeValidation(env, policy):
+def iterativeValidation(policy, env_name):
+    # Follow apex.py and use an env_factory, or expect an env on input?
 
+    #if env_name is None:
+    #    env = env_factory(policy.env_name, speed=speed, state_est=state_est, clock_based=clock_based)()
+    #else:
+    #    env = env_factory(env_name, speed=speed, state_est=state_est, clock_based=clock_based)()
+
+    # This stuff was ripped out from Jonah's dynamics randomization.
+    # We are leaving it as is while building the infrastructure for testing.
+    # Future plan: Put all the damp/mass ranges into a tuple and iterate through,
+    # at each iteration running a test with the current parameter randomized, and all
+    # others default. Then log results and output in a sane and useful way.
     damp = env.default_damping
     weak_factor = 0.5
     strong_factor = 1.5
@@ -74,3 +85,93 @@ def iterativeValidation(env, policy):
     env.sim.set_body_mass(np.clip(mass_noise, 0, None))
     env.sim.set_body_ipos(com_noise)
     env.sim.set_ground_friction(np.clip(fric_noise, 0, None))
+
+#nbody layout:
+# 0:  worldbody (zero)
+# 1:  pelvis
+
+# 2:  left hip roll 
+# 3:  left hip yaw
+# 4:  left hip pitch
+# 5:  left achilles rod
+# 6:  left knee
+# 7:  left knee spring
+# 8:  left shin
+# 9:  left tarsus
+# 10:  left heel spring
+# 12:  left foot crank
+# 12: left plantar rod
+# 13: left foot
+
+# 14: right hip roll 
+# 15: right hip yaw
+# 16: right hip pitch
+# 17: right achilles rod
+# 18: right knee
+# 19: right knee spring
+# 20: right shin
+# 21: right tarsus
+# 22: right heel spring
+# 23: right foot crank
+# 24: right plantar rod
+# 25: right foot
+
+
+# qpos layout
+# [ 0] Pelvis x
+# [ 1] Pelvis y
+# [ 2] Pelvis z
+# [ 3] Pelvis orientation qw
+# [ 4] Pelvis orientation qx
+# [ 5] Pelvis orientation qy
+# [ 6] Pelvis orientation qz
+# [ 7] Left hip roll         (Motor [0])
+# [ 8] Left hip yaw          (Motor [1])
+# [ 9] Left hip pitch        (Motor [2])
+# [10] Left achilles rod qw
+# [11] Left achilles rod qx
+# [12] Left achilles rod qy
+# [13] Left achilles rod qz
+# [14] Left knee             (Motor [3])
+# [15] Left shin                        (Joint [0])
+# [16] Left tarsus                      (Joint [1])
+# [17] Left heel spring
+# [18] Left foot crank
+# [19] Left plantar rod
+# [20] Left foot             (Motor [4], Joint [2])
+# [21] Right hip roll        (Motor [5])
+# [22] Right hip yaw         (Motor [6])
+# [23] Right hip pitch       (Motor [7])
+# [24] Right achilles rod qw
+# [25] Right achilles rod qx
+# [26] Right achilles rod qy
+# [27] Right achilles rod qz
+# [28] Right knee            (Motor [8])
+# [29] Right shin                       (Joint [3])
+# [30] Right tarsus                     (Joint [4])
+# [31] Right heel spring
+# [32] Right foot crank
+# [33] Right plantar rod
+# [34] Right foot            (Motor [9], Joint [5])
+
+# qvel layout
+# [ 0] Pelvis x
+# [ 1] Pelvis y
+# [ 2] Pelvis z
+# [ 3] Pelvis orientation wx
+# [ 4] Pelvis orientation wy
+# [ 5] Pelvis orientation wz
+# [ 6] Left hip roll         (Motor [0])
+# [ 7] Left hip yaw          (Motor [1])
+# [ 8] Left hip pitch        (Motor [2])
+# [ 9] Left knee             (Motor [3])
+# [10] Left shin                        (Joint [0])
+# [11] Left tarsus                      (Joint [1])
+# [12] Left foot             (Motor [4], Joint [2])
+# [13] Right hip roll        (Motor [5])
+# [14] Right hip yaw         (Motor [6])
+# [15] Right hip pitch       (Motor [7])
+# [16] Right knee            (Motor [8])
+# [17] Right shin                       (Joint [3])
+# [18] Right tarsus                     (Joint [4])
+# [19] Right foot            (Motor [9], Joint [5])
