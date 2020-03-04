cd4dc13553994c48e8907622a89d46cdf67bc507
Jonah Siekmann <siekmanj@oregonstate.edu>
2020-03-03 19:23:17 -0800

    fixed ngeom bug

--- cassie/cassiemujoco/cassiemujoco.py
+++ cassie/cassiemujoco/cassiemujoco.py
@@ -204,13 +204,13 @@ class CassieSim:
         cassie_sim_set_body_ipos(self.c, c_arr)
 
     def set_geom_friction(self, data):
-        c_arr = (ctypes.c_double * 3)()
+        c_arr = (ctypes.c_double * (self.ngeom * 3))()
 
-        if len(data) != 3:
+        if len(data) != self.ngeom * 3:
            print("SIZE MISMATCH SET_GEOM_FRICTION()")
            exit(1)
 
-        for i in range(3):
+        for i in range(self.ngeom * 3):
           c_arr[i] = data[i]
 
         cassie_sim_set_geom_friction(self.c, c_arr)
