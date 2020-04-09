9b6221d245952fe72a2c71ac9128c97701c28e1a
JohnNWarila <johnwarila@gmail.com>
2020-04-09 11:16:50 -0700

    renamed script

---
+++ tools/eval_sensitivity.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def eval_policy():
+    # Load from policy file
+
+    # Loop through all params, for each set a high and a low, testing each.
+    # Then reset the param and move to the next. Measure time to death, and
+    # a bool representing if it made it to the end.
+
+
+    # Print a report to the screen and the filesystem. Show death percent and
+    # experiment data.
+    pass
