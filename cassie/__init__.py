66abbf5a082ad4de047b360a3da65dcc0283b07d
Jeremy Dao <daoje@oregonstate.edu>
2021-02-24 01:32:19 -0800

    added turn no orientadd env to init

--- cassie/__init__.py
+++ cassie/__init__.py
@@ -12,7 +12,7 @@ from .cassie_mininput_env import CassieEnv_mininput
 from .cassie_mininput_vel_sidestep_env import CassieEnv_mininput_vel_sidestep
 from .cassie_min_example import CassieEnv_Min
 from .cassie_turn import CassieEnv_turn
-
+from .cassie_turn_no_orientadd import CassieEnv_turn_no_orientadd
 
 from .cassiemujoco import *
 
